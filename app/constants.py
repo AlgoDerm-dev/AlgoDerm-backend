17aafee9e6fa42b62f0ec55c37eb724682df8ad5
JaiGhanekar <jaighanekar1224@gmail.com>
2017-03-24 23:11:20 -0500

    removed constants

--- app/constants.py
+++ app/constants.py
@@ -1,15 +1,12 @@
 import os
 #use os.environ.get('environmental variable')
-DATABASE_URL = 'mysql+pymysql://root:@localhost:3306/AlgoDerm'
-#mysql+pymysql://<username>:<password>@<host>
-SECRET_KEY = 'its_a_secret'
+# DATABASE_URL = 'mysql+pymysql://root:@localhost:3306/AlgoDerm'
+# #mysql+pymysql://<username>:<password>@<host>
+# SECRET_KEY = 'its_a_secret'
 
-BUCKET_NAME = 'algoderm-dev-s3'
 
-# Access Key ID:
-AWS_KEY = 'AKIAIJD4UIHWHL24CBTA'
-# Secret Access Key:
-AWS_SECRET = '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'
+
+
 
 #allowed file extentions
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
